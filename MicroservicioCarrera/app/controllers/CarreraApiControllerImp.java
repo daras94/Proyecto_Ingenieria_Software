3a96a5c38a4b5e34122ad58776186a2f28c1a1dd
davidgarciao <davidgarciao@users.noreply.github.com>
2017-12-18 21:55:21 +0100

    carrerasGet

--- MicroservicioCarrera/app/controllers/CarreraApiControllerImp.java
+++ MicroservicioCarrera/app/controllers/CarreraApiControllerImp.java
@@ -1,12 +1,17 @@
 package controllers;
 
 import apimodels.Carrera;
+import static conexiondb.BBDD.conectar;
+import static conexiondb.BBDD.conexion;
+import static conexiondb.BBDD.consulta_BDD;
 
 import play.mvc.Http;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.io.FileInputStream;
+import java.sql.ResultSet;
+import java.util.HashSet;
 import javax.validation.constraints.*;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaPlayFrameworkCodegen", date = "2017-12-18T17:04:08.877Z")
 
@@ -38,7 +43,46 @@ public class CarreraApiControllerImp implements CarreraApiControllerImpInterface
     @Override
     public List<Carrera> carrerasGet() throws Exception {
         //Do your magic!!!
-        return new ArrayList<Carrera>();
-    }
+        Carrera carrera= new Carrera();
+        ArrayList <Carrera> ArrayCarreras= new ArrayList <>();
+        try{
+        conectar();
+        
+        String sql= "SELECT * From Carreras";
+        ResultSet resultado = consulta_BDD(sql);
+        
+        if (resultado.next()){
+            String nombre = resultado.getString("nombre");
+            int cod_carrera = resultado.getInt("cod_carrera");
+            String facultad = resultado.getString("facultad");
+            int num_cred_opt = resultado.getInt("num_cred_opt");
+            int num_cred_tran = resultado.getInt("num_cred_tran");
+            int num_cred_obl = resultado.getInt("num_cred_obl");
 
+            carrera.setNombre(nombre);
+            carrera.setCodigo(cod_carrera);
+            carrera.setFacultad(facultad);
+            carrera.setNumeroCreditosObligatorios(num_cred_obl);
+            carrera.setNumeroCreditosOptativos(num_cred_opt);
+            carrera.setNumeroCreditosTransversales(num_cred_obl);
+            
+            System.out.println(resultado.toString());
+        }
+        conexion.close();
+        return ArrayCarreras;
+            }
+        catch(Exception e){
+                System.out.println(e.toString());
+                return null;
+            
+            
+            
+            
+            
+        }
+        
+        
+    
+    }                
 }
+
