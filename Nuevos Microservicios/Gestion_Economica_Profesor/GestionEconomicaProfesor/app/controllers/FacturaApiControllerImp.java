78aebd93d475596f57391a82df3aaf3948dd379e
davidgarciao <davidgarciao@users.noreply.github.com>
2018-01-13 19:41:52 +0100

    Gestion Economica Profesor(Facturas)

--- Nuevos Microservicios/Gestion_Economica_Profesor/GestionEconomicaProfesor/app/controllers/FacturaApiControllerImp.java
+++ Nuevos Microservicios/Gestion_Economica_Profesor/GestionEconomicaProfesor/app/controllers/FacturaApiControllerImp.java
@@ -1,20 +1,50 @@
 package controllers;
 
 import apimodels.Factura;
+import static conexionbbdd.BBDD.conectar;
+import static conexionbbdd.BBDD.consulta_BDD;
 
 import play.mvc.Http;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.io.FileInputStream;
+import java.math.BigDecimal;
+import java.sql.ResultSet;
 import javax.validation.constraints.*;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaPlayFrameworkCodegen", date = "2018-01-04T20:12:09.365Z")
 
 public class FacturaApiControllerImp implements FacturaApiControllerImpInterface {
     @Override
     public List<Factura> facturasGet(String NIF, Integer mes, Integer ao) throws Exception {
-        //Do your magic!!!
-        return new ArrayList<Factura>();
+        ArrayList<Factura> facturas= new ArrayList<>();
+        try{
+            conectar();
+            String sql = "SELECT * FROM reservaprofesor WHERE Profesor_NIF = "+NIF+ " AND MONTH(fecha)= "+mes+" AND YEAR(fecha)= "+ao;
+            ResultSet resultado = consulta_BDD(sql);
+             
+            while (resultado.next()){
+                int ID_Espacio= resultado.getInt("ID_Espacio");
+                String fecha= resultado.getString("fecha");
+                int hora= resultado.getInt("hora");
+                Factura factura= new Factura();
+                factura.setEspacio(String.valueOf(ID_Espacio));
+                factura.setHora(String.valueOf(hora));
+                factura.setFecha(fecha);
+                sql= "SELECT precio_alquiler FROM espacio WHERE codigo = "+ID_Espacio;
+                ResultSet resultado_espacio = consulta_BDD(sql);
+                if (resultado_espacio.next()){
+                    double precio= resultado_espacio.getDouble("precio_alquiler");
+                    factura.setCoste(BigDecimal.valueOf(precio));
+                }
+                facturas.add(factura);
+            }
+        }
+        catch (Exception e){
+            return null;
+        }
+        return facturas;
     }
+    
 
 }
