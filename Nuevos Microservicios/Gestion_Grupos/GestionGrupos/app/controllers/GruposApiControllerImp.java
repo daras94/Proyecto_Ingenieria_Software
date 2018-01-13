bc20ba58277251e9707bc261c054a3994b4dbdb7
Ruben <33247897+Rubydrag@users.noreply.github.com>
2018-01-13 19:23:36 +0100

    Recuperacion gestion grupos
    Debido a su anterior borrado por un commit

--- Nuevos Microservicios/Gestion_Grupos/GestionGrupos/app/controllers/GruposApiControllerImp.java
+++ Nuevos Microservicios/Gestion_Grupos/GestionGrupos/app/controllers/GruposApiControllerImp.java
@@ -9,14 +9,45 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.io.FileInputStream;
+import java.sql.ResultSet;
 import javax.validation.constraints.*;
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaPlayFrameworkCodegen", date = "2018-01-12T19:17:10.953Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaPlayFrameworkCodegen", date = "2018-01-13T15:37:03.349Z")
 
 public class GruposApiControllerImp implements GruposApiControllerImpInterface {
     @Override
     public List<Grupo> getGruposGet() throws Exception {
-        //Do your magic!!!
-        return new ArrayList<Grupo>();
+        String query = "SELECT id_grupo, Cod_asignatura, tipo FROM Grupo WHERE id_grupo NOT IN (SELECT grupo_id FROM Profesor_Grupo);";
+        ArrayList<Grupo> grupos = new ArrayList<Grupo>();
+        
+        try{
+            conectar();
+        
+            ResultSet respuesta = consulta_BDD(query);
+            while(respuesta.next()){
+                int codAsignatura = respuesta.getInt("Cod_asignatura");
+                String query2 = "SELECT nombre FROM Asignatura WHERE Cod_asignatura = "+codAsignatura+";";
+                ResultSet respuesta2 = consulta_BDD(query2);
+                if(respuesta2.next()){
+                    int idGrupo = respuesta.getInt("id_grupo");
+                    String tipo = respuesta.getString("tipo");
+                    String asignatura = respuesta2.getString("nombre");
+                    Grupo grupo = new Grupo();
+                    grupo.setAsignatura(asignatura);
+                    grupo.setId(idGrupo);
+                    grupo.setTipo(tipo);
+                    grupos.add(grupo);
+                }
+            }
+        }
+        catch (Exception e){
+            System.out.println(e.toString());
+        }
+        finally{
+            if(conexion!=null){
+                conexion.close();
+            }
+            return grupos;
+        }
     }
 
     @Override
@@ -42,6 +73,7 @@ public class GruposApiControllerImp implements GruposApiControllerImpInterface {
                 conexion.close();
             }
         }
+        
     }
 
 }
